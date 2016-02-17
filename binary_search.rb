3c64811cc9b0ac0bb4acbb4bfab07dd24591826c
Saurav <csaurav80@gmail.com>
2016-02-17 08:39:56 +0000

    binary search implementation

---
+++ binary_search.rb
@@ -0,0 +1,23 @@
+# Binary Search implementation
+
+
+def binary_search(array, value, from=0, to=nil)
+  if to.eql? nil
+    to = array.length - 1
+  end
+  return 'element not present' if value < array.first or value > array.last
+  mid = (from + to)/2
+  if value < mid
+    return binary_search(array, value, from, mid - 1)
+  elsif value > mid
+    return binary_search(array, value, mid +1, to)
+  else
+    return mid
+  end
+end
+
+
+a = (1..100).to_a
+
+puts binary_search(a, 80)
+
